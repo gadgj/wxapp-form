183c1f33afc423d710e5ddf63dd4351c778603e3
King <ziqingzhao20@gmail.com>
2021-01-20 01:01:05 +0800

    完善表单选项

--- pages/index/index.js
+++ pages/index/index.js
@@ -38,59 +38,38 @@ Page({
         defaultIdx:0,//默认选择索引
         disabled:false,
         isRequired:true,
-        range:[
-          {
-            id: 0,
-            name: '男'
-          },
-          {
-            id: 1,
-            name: '女'
-          },
-        
-        ]
+        range:['男','女']
       },
       {
         type: 'multi_picker',
-        id: 'test',
-        lable: '班别',
+        id: 'zhiCheng',
+        lable: '当前职称',
         defaultIdx:[0, 0],//默认选择索引
         disabled:false,
         isRequired:true,
-        multi_range:[//存储x轴和某个y轴，初始化为第一个x轴＋第一个y_range --King 2021.01.19
-          ['计算机科学与技术类', '电气信息类'],
-          ['16计科一', '17计科一', '18计科一']
+        multi_range:[//存储x轴和某个可变的y轴，初始化为第一个x轴＋第一个y轴y_range[0] --King 2021.01.19
+          ['医师系列','护理系列','药师系列','技师系列','中医医师系列','中西医结合医师系列','中医技师系列','中医药师系列','见习系列','工程系列','研究系列','教学系列','检验师系列','统计系列','计生系列','其他系列'],
+          ['主任医师','副主任医师','主治医师','医师','主管医师','医士','助理医师']
         ],
-        y_range: [//y轴
-          ['16计科一', '17计科一' , '18计科一'],
-          ['16电器一','17电器一']
+        y_range:[//所有y轴
+          ['主任医师','副主任医师','主治医师','医师','主管医师','医士','助理医师'], 
+          ['主任护师','副主任护师','主管护师','护师','护士'], 
+          ['主任药师','副主任药师','主管药师','药剂师','药剂士'], 
+          ['主任技师','副主任技师','主管技师','技师','技士'], 
+          ['中医主任医师','中医副主任医师','中医主治医师','中医医师','中医医士'],
+          ['中西医结合主任医师','中西医结合副主任医师','中西医结合主治医师','中西医结合医师'],
+          ['中医主任技师','中医副主任技师','中医主管技师','中医技师','中医技士'],
+          ['中医主任药师','中医副主任药师','中医主治药师','中医药师','中医药士'],
+          ['见习护师','见习检验师','见习检验士 ','见习技师','见习技士','见习药剂师','见习医师','见习医士','见习护士'],
+          ['高级工程师','工程师','助理工程师 ','技术员'],
+          ['研究员','副研究员','助理研究员 ','实习研究员'],
+          ['教授','副教授','讲师 ','助教'],
+          ['主任检验师','副主任检验师','主管检验师 ','检验师','检验士'],
+          ['高级统计师','统计师','助理统计师','统计员'],
+          ['无职称'],
+          ['生殖健康高级咨询师','生殖健康咨询师','生殖健康助理咨询师','生殖健康咨询员','生殖健康助理咨询员']
         ]
-      },
-      {
-        type: 'picker',
-        id: 'zhiCheng',
-        lable: '当前职称',
-        defaultIdx:0,//默认选择索引
-        disabled:false,
-        isRequired:true,
-        range:[
-          {
-            id: 0,
-            name: '医师系列'
-          },
-          {
-            id: 1,
-            name: '护理系列'
-          },
-          {
-            id: 2,
-            name: '药师系列'
-          },
-          {
-            id: 3,
-            name: '技师系列'
-          }        
-        ]
+
       },
       {
         type: 'date',
@@ -131,49 +110,34 @@ Page({
         defaultIdx:0,//默认选择索引
         disabled:false,
         isRequired:true,
-        range:[
-          {
-            id: 0,
-            name: '护理科'
-          },
-          {
-            id: 1,
-            name: '心血管科'
-          },
-          {
-            id: 2,
-            name: '呼吸内科'
-          },
-          {
-            id: 3,
-            name: '消化内科'
-          }        
-        ]
+        range:['护理科','心血管科','呼吸内科','消化内科','肾内科','神经内科','血液内科','内分泌科','风湿免疫科','精神科','血液肿瘤科','脊柱骨病科','普外科','心胸外科','肝胆外科','泌尿外科','神经外科','烧伤整形外科','妇产科','儿科','急诊科','重症医学科','口腔科','耳鼻喉科','眼科','皮肤科','感染科','中医科','康复科','麻醉科','超声科','影像科','体检科','药学部','输血科','病理科','医务部','后勤保障部','财务部','信息技术部','全科','医学工程部','医院感染控制科','人力资源部','其他','关节创伤外科']
       },
       {
-        type: 'picker',
+        type: 'multi_picker',
         id: 'zhuanYe',
         lable: '专业',
-        defaultIdx:0,//默认选择索引
+        defaultIdx:[0,0],//默认选择索引
         disabled:false,
         isRequired:true,
-        range:[
-          {
-            id: 0,
-            name: '护理学'
-          },
-          {
-            id: 1,
-            name: '临床医学'
-          },
-          {
-            id: 2,
-            name: '急救医学'
-          },
-          {
-            id: 3,
-            name: '预防医学'
-          }        
+        multi_range:[
+          ['护理学','临床医学','急救医学','预防医学','中医药学','医学生物工程学','基础医学','医药信息学','软科学','药学','其他医学专业','管理专业','其他'],
+          ['护理学']
+        ],
+        y_range:[
+          ['护理学'], 
+          ['主任护师','副主任护师','主管护师','护师','护士'], 
+          ['内科学','外科学','妇产科学','儿科学','眼科学','麻醉学','临床检验学','神经病学','医学影像学','精神病学','理疗学','临床肿瘤学','口腔医学','耳鼻咽喉科学','放射治疗学','全科医学','传染病学','皮肤病学','法医学','老年医学','运动医学'], 
+          ['急救医学'], 
+          ['地方病学','寄生虫学','卫生标准','环境卫生学','放射卫生学','微生态学','流行病学','卫生检验（疫）学','病毒学','营养和食品卫生学','微生物学','儿少卫生学','劳动卫生和职业病'],
+          ['针灸学','中医临床','中医文献学','中医基础理论','中西医结合','中药学'],
+          ['医用电子技术','医用激光','人造器官','生化仪器','生物力学','医疗器械','医学仪器','流体力学','医用新材料'],
+          ['组织胚胎学','医用物理学','医用化学','医学免疫学','人体解剖学','生物化学','医学遗传学','生理学','病理生理学','航空航海医学','医用生物学','生物物理学','病理学','基础肿瘤学','实验动物'],
+          ['医学生物学模式或识别','生物医学信号处理','医学人工智能和专家系统 ','医学统计学','医院信息系统','数字医学影像学','计算分子生物学','医学图书情报处理技术'],
+          ['科研管理','卫生经济','卫生事业管理 ','卫生统计学','图书馆学','医院管理','健康教育','社会医学'],
+          ['制药工艺学','微生物药学','药物检定 ','药物化学','药剂学','生物制品','药理学','生药学','药用植物学'],
+          ['其他医学专业'],
+          ['管理专业'],
+          ['其他'],  
         ]
       },
       {
@@ -312,12 +276,12 @@ Page({
           tapId: this.data.tapId,
           idcard: e.detail.idcard && e.detail.idcard.value,
           xingMing: e.detail.xingMing && e.detail.xingMing.value,
-          xingBie: e.detail.xingBie && e.detail.xingBie.original.range[e.detail.xingBie.idx].name,
-          zhiCheng: e.detail.zhiCheng && e.detail.zhiCheng.original.range[e.detail.zhiCheng.idx].name,          
+          xingBie: e.detail.xingBie && e.detail.xingBie.data,
+          zhiCheng: e.detail.zhiCheng && e.detail.zhiCheng.data,          
           pingDingShiJian: e.detail.pingDingShiJian && e.detail.pingDingShiJian.startDate,
           pinRenNianDu: e.detail.pinRenNianDu && e.detail.pinRenNianDu.value,
-          keShi: e.detail.keShi && e.detail.keShi.original.range[e.detail.keShi.idx].name,
-          zhuanYe: e.detail.zhuanYe && e.detail.zhuanYe.original.range[e.detail.zhuanYe.idx].name,
+          keShi: e.detail.keShi && e.detail.keShi.data,
+          zhuanYe: e.detail.zhuanYe && e.detail.zhuanYe.data,
           shouJi: e.detail.shouJi && e.detail.shouJi.value,
           xueFenKa: e.detail.xueFenKa && e.detail.xueFenKa.value,
           youXiang: e.detail.youXiang && e.detail.youXiang.value,
